6afc52a9d461df5fce980ec262b163038b449e0b
tmytnk0914-creator <tmy.tnk0914@gmail.com>
2026-07-02 00:58:01 +0900

    chore: GitHub Pages デプロイ設定を追加 (base URL + Actions workflow)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Vite設定ファイル
+// base: GitHub Pagesのリポジトリ名に合わせる
 export default defineConfig({
   plugins: [react()],
+  base: '/NewApp/',
 })
